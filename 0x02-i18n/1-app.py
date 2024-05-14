b4e013fa7744a042dde2ee8c70d2242d0592b64b
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-05-14 11:00:19 +0000

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,37 +1,26 @@
 #!/usr/bin/env python3
-"""
-Module for starting a basic Flask web application.
-"""
+"""A babel and flask app"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
 
-# Create Flask app instance
-app = Flask(__name__)
-
-# Instantiate the Babel object and store it in a module-level variable named babel
-babel = Babel(app)
-
-# Config class to set available languages and default locale/timezone for the app
 class Config:
     """
-    Config class to set available languages and default locale/timezone for the app.
+    Setting the languages and the locale and timezone
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-# Use Config class as config for the Flask app
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
-# Route to render the index.html template
 @app.route('/')
-def index():
-    """
-    Renders the index.html template.
-    """
-    return render_template('1-index.html', title="Welcome to Holberton", header="Hello world")
+def hello_world():
+    """to run on the website"""
+    return render_template('1-index.html')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-
+    app.run(host='0.0.0.0', port='5000', debug=True)
