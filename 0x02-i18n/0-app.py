b101169fcd6e637893a6dfea41a5d9d5cfcc7a5d
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-05-14 11:07:39 +0200

    1-app.py'

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for starting a basic Flask web application.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Renders the index.html template.
+    """
+    return render_template('0-index.html', title="Welcome to Holberton", header="Hello world")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
